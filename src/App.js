0541f894e6e5762c306463a147f7a6f25e9b0bd7
ShannonDenton <denton.shannon7@gmail.com>
2022-10-12 18:01:11 -0500

    Added search alert

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>
+          <small>
+            {" "}
+            Coded by{" "}
+            <a href="https://shannon-denton.com/" target="_blank">
+              {" "}
+              Shannon Denton
+            </a>{" "}
+            and is open-sourced on Github.
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
