65e7aa42e7dcef8ffc35c167403b4e5275c15ad9
ShannonDenton <denton.shannon7@gmail.com>
2022-10-13 16:20:29 -0500

    Rendered the word meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,20 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import Results from "./Results";
+
 import axios from "axios";
 
 export default function App() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
     console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   function Search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
 
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
@@ -26,6 +29,7 @@ export default function App() {
       <form onSubmit={Search}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
