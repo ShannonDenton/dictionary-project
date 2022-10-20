19fd82c230e75f0d5be845b3a5f32f5f17f4cc55
ShannonDenton <denton.shannon7@gmail.com>
2022-10-20 16:02:45 -0500

    Added style to page

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,32 +4,51 @@ import Results from "./Results";
 
 import axios from "axios";
 
-export default function App() {
-  let [keyword, setKeyword] = useState("");
+export default function Dictionary(props) {
+  let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
     setResults(response.data[0]);
   }
 
-  function Search(event) {
-    event.preventDefault();
-
+  function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
   }
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
 
-  return (
-    <div className="Dictionary">
-      <form onSubmit={Search}>
-        <input type="search" onChange={handleKeywordChange} />
-      </form>
-      <Results results={results} />
-    </div>
-  );
+  function load() {
+    setLoaded(true);
+    search();
+  }
+
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <section>
+          <h1>Wordfinder</h1>
+          <form onSubmit={handleSubmit}>
+            <input type="search" onChange={handleKeywordChange} />
+          </form>
+          <div className="hint">
+            Suggested words: cake, pink, chocolate, pasta, paris
+          </div>
+          <Results results={results} />
+        </section>
+      </div>
+    );
+  } else {
+    load();
+    return "Loading";
+  }
 }
--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Phonetic.css";
 
 export default function Phonetic(props) {
   console.log(props.phonetic);
@@ -8,7 +9,6 @@ export default function Phonetic(props) {
         {" "}
         Listen
       </a>
-      <br />
       {props.phonetic.text}
     </div>
   );
